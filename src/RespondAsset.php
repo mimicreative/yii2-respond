2b30ecc9074f489832f8ff28f78858d4ee1389a5
Haqqi <me@haqqi.net>
2016-10-22 19:29:09 +0700

    Finalize respond asset

---
+++ src/RespondAsset.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace mimicreative\respond;
+
+class RespondAsset extends \yii\web\AssetBundle {
+  
+  /**
+  * @inherit
+  */
+  public $sourcePath = '@bower/respond';
+  
+  /**
+  * @inherit
+  */
+  public $js = [
+    'dest/respond.min.js',
+  ];
+  
+  public $jsOptions = [
+    'condition' => 'lte IE9',
+    'position' => \yii\web\View::POS_HEAD
+  ];
+  
+  /**
+  * Initializes the bundle.
+  * Set publish options to copy only necessary files (in this case css and font folders)
+  */
+  public function init()
+  {
+     parent::init();
+
+     $this->publishOptions['beforeCopy'] = function ($from, $to) {
+         return preg_match('%(/|\\\\)dest%', $from);
+     };
+  }
+}
+
